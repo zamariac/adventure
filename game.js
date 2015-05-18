8e016910d304144785d15aab54fc2bea97c9844a
Maria Zamora <Zamora.Mariac@gmail.com>
2015-05-18 14:45:11 -0500

    final

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function* chooseYourAdventure() {
+function* chooseYourAdventure(){
 	//yield adventure.start('Welcome to an awesome adventure!');
 	//yield adventure.say('Hello world!');
 	//var name = yield adventure.ask('What is your name?');
@@ -7,13 +7,21 @@ function* chooseYourAdventure() {
 	//adventure.end('That\'s all folks!');
 
 
-yield adventure.start ('We\'re going to Paris!');
+yield adventure.start('We\'re going to Paris!');
 yield adventure.say('There will be lots of sightseeing and dining!');
 var myName= yield adventure.ask('Before we begin, Comment tu t\'apelles?');
 yield adventure.say('Bonjour '+myName+', Von Voyage!');
-var myObject = yield adventure.choose ('What are you going to bring with you?', 'A sketchbook', 'A camera'); 
+var myObject = yield adventure.choose('What are you going to bring with you?', 'A sketchbook', 'A camera'); 
 yield adventure.say( 'You chose a '+myObject);
-var userContinue = yield adventure.choose (
+
+	if(myObject === 'A sketchbook'){
+		yield adventure.say ('You live in the moment!');
+	}
+	 else if (myObject === 'A camera') {
+	 	yield adventure.say ('You don\'t live in the moment!');
+	 }
+
+var userContinue = yield adventure.choose(
 	'Would you like to continue?',
 	'Oui!',
 	'Non!' 
@@ -31,7 +39,7 @@ var userContinue = yield adventure.choose (
 	}
 
 	
-
+}
 
 
 
